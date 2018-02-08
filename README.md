2b812245be23ed5d828adef16709027fd0a40026
joewong <joewong@joes-air>
2018-02-08 13:38:03 -0600

    Added Data Model and updated README.md

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# CS4330 Challenge 1: Checkerboard
+# CS4330
+## Challenge 1: Checkerboard
 This challenge required us to create a javaFX + Scenebuilder 2.0 application that would fill in a checkerboard and be dynamically sized with the window.
 The menu contains options for changing the grid dimensions as well as board colors.
+
+## Challenge 3: Checkerboard Data Model
+This challenge required us to think about how we would implement a data model for the checkers game so that UI elements would not have to be used to determine the state of the game.
---
+++ checkerboard/src/checkerboard/CheckerboardMain.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package checkerboard;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author joewong
+ */
+public class CheckerboardMain extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("checkerboardFXML.fxml"));
+        Parent root = (Parent)loader.load();
+        checkerboardFXMLController boardController = loader.getController();
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+        System.out.println("About to start");
+        boardController.start(stage);    
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
